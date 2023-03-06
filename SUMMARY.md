aba645a449629fa7e8930fd97ddf0b82f3820939
Thomas <2854577982@qq.com>
2023-03-06 09:28:43 +0800

    我的第一次上传

---
+++ SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+* [Introduction](README.md)
+* [安装gitbook](learn/安装问题.md)
+* ​
+
---
+++ book.js
@@ -0,0 +1,18 @@
+module.exports={
+    //书籍信息
+    "title":"书名",
+    "description":"描述",
+    "isbn":"图书编号",
+    "author":"作者",
+    "lang":"zh-cn",
+
+    //插件表列
+    "plugins": [],
+
+    //插件全局配置
+    "pluginsConfig":{},
+    //模块变量
+    variables:{
+        //自定义
+    },
+};
